ef75cdf41e9421b855651f6d1b6e9fb9308f23da
kalychen <xxkalychen@163.com>
2018-09-16 03:57:18 +0800

    构建项目基本雏形

---
+++ src/main/java/com/chris/data/annotation/query/PageParam.java
@@ -0,0 +1,17 @@
+package com.chris.data.annotation.query;
+
+import java.lang.annotation.*;
+
+/**
+ * YuedaoXingApi
+ * com.ydx.api.libs.chris.annotation
+ * Created by Chris Chen
+ * 2018/1/12
+ * Explain:分页
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface PageParam {
+}
